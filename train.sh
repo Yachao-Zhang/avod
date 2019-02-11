40b5203ff499d14a146c90a13c9d8de5c7c17b77
root <fredrik@vatsendvik.com>
2019-02-11 13:58:31 +0000

    adding config for comparsion

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 avod/experiments/run_training.py --pipeline_config=avod/configs/bev_test.config --device='0' --data_split='train'
+python3 avod/experiments/run_training.py --pipeline_config=avod/configs/bev_test_cars.config --device='0' --data_split='train'
