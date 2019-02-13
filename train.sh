0ae87b65c8d9a810919cdcee20967b268bee90b3
root <fredrik@vatsendvik.com>
2019-02-13 13:38:20 +0000

    retesting for cars

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 avod/experiments/run_training.py --pipeline_config=avod/configs/bev_test_people.config --device='1' --data_split='train'
+python3 avod/experiments/run_training.py --pipeline_config=avod/configs/bev_test_cars.config --device='1' --data_split='train'
