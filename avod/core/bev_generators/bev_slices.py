bed737f1b0ede8adca61cb455ad310e873b2b3e1
Fredrik00 <Fredrik@Vatsendvik.com>
2019-03-04 16:25:00 +0100

    typo

--- avod/core/bev_generators/bev_slices.py
+++ avod/core/bev_generators/bev_slices.py
@@ -112,7 +112,7 @@ class BevSlices(bev_generator.BevGenerator):
                                    voxel_grid_2d.num_divisions[2]))
 
             # Only update pixels where voxels have values
-            variance_map[voxel_indices[:, 0], voxel_indices[:, 1]] = np.asarray(np.multiply(voxel_grid_2d.variance, voxel_grid_2d.num_points_in_voxel))
+            variance_map[voxel_indices[:, 0], voxel_indices[:, 1]] = np.asarray(np.multiply(voxel_grid_2d.variance, voxel_grid_2d.num_pts_in_voxel))
             variance_map /= np.max(variance_map)
 
 
