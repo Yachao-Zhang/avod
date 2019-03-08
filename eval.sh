2f6ac7f46666379e2f126f02dca884f15bcefefa
root <fredrik@vatsendvik.com>
2019-03-08 11:30:24 +0000

    BEV CONFIG RELEASE: tested and ready for merge

--- eval.sh
+++ eval.sh
@@ -1,2 +1,2 @@
-python3 avod/experiments/run_evaluation.py --pipeline_config=avod/data/outputs/pyramid_cars_example_copy/pyramid_cars_example_copy.config --device='0' --data_split='val'
+python3 avod/experiments/run_evaluation.py --pipeline_config=avod/data/outputs/max_min_density_cars/max_min_density_cars.config --device='0' --data_split='val'
 
