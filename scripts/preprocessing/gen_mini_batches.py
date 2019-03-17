21f9f0f27415ae4ce33644cedaae2a7c6b85a945
root <fredrik@vatsendvik.com>
2019-03-17 15:33:06 +0000

    testing randomly resampled training file for balancing

--- scripts/preprocessing/gen_mini_batches.py
+++ scripts/preprocessing/gen_mini_batches.py
@@ -88,11 +88,11 @@ def main(dataset=None):
     car_dataset_config_path = avod.root_dir() + \
         '/configs/mb_preprocessing/rpn_cars.config'
     ped_dataset_config_path = avod.root_dir() + \
-        '/configs/mb_preprocessing/rpn_pedestrians.config'
+        '/configs/mb_preprocessing/pedestrians_max_density.config'
     cyc_dataset_config_path = avod.root_dir() + \
         '/configs/mb_preprocessing/rpn_cyclists.config'
     ppl_dataset_config_path = avod.root_dir() + \
-        '/configs/mb_preprocessing/rpn_people.config'
+        '/configs/mb_preprocessing/people_max_density.config'
     all_dataset_config_path = avod.root_dir() + \
         '/configs/mb_preprocessing/test_all.config'
 
@@ -103,9 +103,9 @@ def main(dataset=None):
     in_parallel = True
 
     process_car = False   # Cars
-    process_ped = True # Pedestrians
+    process_ped = False # Pedestrians
     process_cyc = False  # Cyclists
-    process_ppl = False   # People (Pedestrians + Cyclists)
+    process_ppl = True   # People (Pedestrians + Cyclists)
     process_all = False # Cars + Pedestrians + Cyclists
 
     # Number of child processes to fork, samples will
