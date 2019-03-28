d159c16980c15d3011b5dd8311a6b2a6dd48dee2
Fredrik <Fredrik@Vatsendvik.com>
2019-03-28 21:07:04 +0100

    Attempt less aggressive clustering

--- scripts/preprocessing/gen_mini_batches.py
+++ scripts/preprocessing/gen_mini_batches.py
@@ -86,7 +86,7 @@ def main(dataset=None):
         return
 
     car_dataset_config_path = avod.root_dir() + \
-        '/configs/mb_preprocessing/carla_cars.config'
+        '/configs/mb_preprocessing/cars_cluster.config'
     ped_dataset_config_path = avod.root_dir() + \
         '/configs/mb_preprocessing/pedestrians_max_density.config'
     cyc_dataset_config_path = avod.root_dir() + \
@@ -104,12 +104,12 @@ def main(dataset=None):
     # Serial vs parallel processing
     in_parallel = True
 
-    process_car = False   # Cars
+    process_car = True   # Cars
     process_ped = False # Pedestrians
     process_cyc = False  # Cyclists
     process_ppl = False   # People (Pedestrians + Cyclists)
     process_all = False # Cars + Pedestrians + Cyclists
-    process_per = True   # Person (Pedestrians + Cyclists joint class)
+    process_per = False   # Person (Pedestrians + Cyclists joint class)
 
     # Number of child processes to fork, samples will
     #  be divided evenly amongst the processes (in_parallel must be True)
